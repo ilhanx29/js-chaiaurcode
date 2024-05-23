19327c5e62032de55768fa9116849a06d9520eb6
Ilhan <mail.ilhanx29@gmail.com>
2024-05-23 14:25:05 +0000

    let var & const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = ilhanx29
+let accountEmail = "mail.ilhanx29@gmail.com"
+var accountPassword = "12345"
+accountCity = "Malda"
+
+// accountId = 2 // not allowed
+
+accountEmail = "dsk9702@gmail.com"
+accountPassword = "212121"
+accountCity = "Howrah"
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity,])
