71ed86fb89f9ef81e46788e411feb8bb70cf8c8d
Ilhan <mail.ilhanx29@gmail.com>
2024-05-24 15:37:47 +0000

    datatypes discussed

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert ( 3 + 3 ) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+    console.log("Ilhan")
+
+
+    let name = "ilhan"
+    let age = 24
+    let isLoggedIn = false
+
+
+    // number => 2 to the power 53
+    // bigint
+    // string => ""
+    // bolean => true/false
+    // null => standalone value
+    // undefined => 
+    // symbol => unique
+
+
+    // object 
+
+    console.log(typeof undefined); // undefined
+    console.log(typeof null); // object
+    
