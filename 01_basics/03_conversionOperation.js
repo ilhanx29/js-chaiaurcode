c7b76714705f916055dcd4e8c03ac6a964965619
Ilhan <mail.ilhanx29@gmail.com>
2024-06-14 04:42:40 +0000

    Complete

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,14 +3,14 @@ let score = 33
 // console.log(typeof score);
 // console.log(typeof(score));
 
-let valueInNumber = Number (score)
+let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
 
 
 // "33" => 33
 // "33abc" => NaN
-// true => 1;; false => 0
+//  true => 1; false => 0
 
 let isLoggedIn = "hitesh"
 
@@ -45,4 +45,9 @@ let str2 = " Ilhan"
 
 let str3 = str1 + str2
 console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
 
